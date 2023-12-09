8d02cda0a1af2d1a024af727f5a44cccedfe0759
Febriana Kusumah <febriana.kusumah@nexcloud.co.id>
2023-12-10 02:43:54 +0700

    init

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { FC } from "react";
+
+interface IMenuItem {
+  onClick: () => void,
+  label: string
+}
+
+const MenuItem: FC<IMenuItem> = ({
+  onClick,
+  label
+}) => {
+  return (
+    <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+      {label}
+    </div>
+  );
+}
+ 
+export default MenuItem;
